3d7ff4e95611111fb3e909e282a6097e0cbe0861
91焦先生 <1292366529@qq.com>
2022-03-28 18:16:45 +0800

    添加源文件更新支持

--- Assets/Scripts/BPath.cs
+++ Assets/Scripts/BPath.cs
@@ -3,12 +3,12 @@ namespace BM
 {
 	public class BPath
 	{
+		public const string Assets_Bundles_SubBundleAssets_mister91jiao__prefab = "Assets/Bundles/SubBundleAssets/mister91jiao.prefab";
 		public const string Assets_Bundles_LoginBG__jpg = "Assets/Bundles/LoginBG.jpg";
 		public const string Assets_Bundles_LoginUI__prefab = "Assets/Bundles/LoginUI.prefab";
 		public const string Assets_Bundles_ResetUI__prefab = "Assets/Bundles/ResetUI.prefab";
 		public const string Assets_Bundles_SubUI__prefab = "Assets/Bundles/SubUI.prefab";
 		public const string Assets_Bundles_xjj__jpg = "Assets/Bundles/xjj.jpg";
-		public const string Assets_Bundles_SubBundleAssets_mister91jiao__prefab = "Assets/Bundles/SubBundleAssets/mister91jiao.prefab";
 		public const string Assets_Scenes_Game__unity = "Assets/Scenes/Game.unity";
 	}
 }
